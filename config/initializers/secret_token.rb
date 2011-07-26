18a6296caffbdb26004209e3cb44937ea6e55e84
Takeru Shigemori <jetspeed4u@gmail.com>
2011-07-26 15:36:36 +0900

    default src

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Multienv::Application.config.secret_token = '56be08d3d146318b28f60ae79b8f0fd6112a89f32815448bb68a54456d5a82af67f4841cb97898b71af38a5c021c5dd349af220c09a3a12b9bb79f3451ef8c94'
